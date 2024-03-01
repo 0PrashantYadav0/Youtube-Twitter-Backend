36818ed3ecdec4f87efded7f7e503e51dc545a5e
Prashant <prashantyadav09783@gmail.com>
2024-03-01 13:13:01 +0530

    fixes

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(`Mongoose Connected to ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.log("MONGODB connection error: " + error);
+    console.log("MONGODB connection error: ");
     process.exit(1);
   }
 }
